a469f9963cb9a6084f1a56621d9c103db09e7052
Matthias Fuchs <61468448+DoctorFuchs@users.noreply.github.com>
2021-06-24 16:13:29 +0000

    clock countdown with cd

--- core/plugins/clock/DOC.py
+++ core/plugins/clock/DOC.py
@@ -3,7 +3,7 @@ from core.new import plugin
 clockplugin = plugin.plugin("clock")
 
 
-@clockplugin.command("c", instance=True)
+@clockplugin.command("time", instance=True)
 def clock(instance):
     """live time clock"""
     import time, datetime
@@ -14,3 +14,21 @@ def clock(instance):
 
     except:
         instance.docprint("", end="\n")
+
+@clockplugin.command("cd", args=True, instance=True)
+def countdown(args, instance):
+	"""countdown"""
+	import time
+	try:
+		times = int(args[1])
+		while True:
+			if times <= 0.0:
+				instance.docprint(f"\rFinished ({int(args[1])})\n", end="")
+				break
+			time.sleep(0.1)
+			times -= 0.1
+			instance.docprint(f"\r{round(times*10)/10}", end="")
+
+	except ValueError:
+		instance.docprint("usage: clock cd <time>")
+	
